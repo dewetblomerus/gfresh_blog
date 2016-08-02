f0deb8eb669b02d8767719182efeb02315bace07
De Wet <dewet@blomerus.org>
2016-08-02 10:25:55 +0200

    some help from rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802074144) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_160_802_074_144) do
+  create_table 'posts', force: :cascade do |t|
+    t.string   'title'
+    t.text     'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
--- spec/features/add_post_spec.rb
+++ spec/features/add_post_spec.rb
@@ -1,13 +1,13 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "adding posts" do
-  it "allows a user to create a prost with a title and body" do
+describe 'adding posts' do
+  it 'allows a user to create a prost with a title and body' do
     visit new_post_path
-    fill_in "Title", with: "My First Post"
-    fill_in "Body", with: "Well, when I first decided to start blogging..."
-    click_on("Create Post")
+    fill_in 'Title', with: 'My First Post'
+    fill_in 'Body', with: 'Well, when I first decided to start blogging...'
+    click_on('Create Post')
     visit posts_path
-    expect(page).to have_content("My First Post")
-    expect(page).to have_content("Well, when I first")
+    expect(page).to have_content('My First Post')
+    expect(page).to have_content('Well, when I first')
   end
 end
