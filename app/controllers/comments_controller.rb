2a9cb948cabe593b45ae0658f1bcd0ac907c6201
De Wet <dewet@blomerus.org>
2016-08-02 19:03:47 +0200

    ajax comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.post = @post
+    @comment.save!
+
+    respond_to do |format|
+      format.html { redirect_to @comment }
+      format.js
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user, :body)
+  end
 end
